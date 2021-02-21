57a609e0550bf8c2be4077eb04f1fa5c8f0e9692
Daniel Giat <danielg@outbrain.com>
2021-02-21 20:50:26 +0200

    👱‍♂️ header

--- todo-list/src/main.ts
+++ todo-list/src/main.ts
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
-// import HeaderComponent from '@/components/layout/Header.vue';
+import HeaderComponent from '@/components/layout/Header.vue';
 import '@/assets/styles/styles.scss';
 
 Vue.config.productionTip = false
 
-// Vue.component('tl-header', HeaderComponent);
+Vue.component('tl-header', HeaderComponent);
 new Vue({
   router,
   vuetify,
